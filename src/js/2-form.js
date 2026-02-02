e42b8d86adc80ce211dbd5bba1da6e987118b948
Marina <marina.miram@gmail.com>
2026-02-02 23:08:51 +0300

    .

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -6,10 +6,6 @@ const formData = {
 const form = document.querySelector(".feedback-form");
 const STORAGE_KEY = "feedback-form-state";
 
-
-form.addEventListener("input", onFormInput);
-form.addEventListener("submit", onFormSubmit);
-
 function onFormInput(event) {
     const { name, value } = event.target;
     if (!name) return;
@@ -17,15 +13,18 @@ function onFormInput(event) {
     localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
 }
 
+form.addEventListener("input", onFormInput);
+form.addEventListener("submit", onFormSubmit);
+
     const savedData = localStorage.getItem(STORAGE_KEY);
     if (savedData) {
         formData = JSON.parse(savedData);
         form.elements.email.value = formData.email || "";
         form.elements.message.value = formData.message || "";
 }
+
 function onFormSubmit(event) {
     event.preventDefault();
-
     if (!formData.email || !formData.message) {
         alert("Fill please all fields");
         return;
